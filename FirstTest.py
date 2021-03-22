706cd57654be0a4a9af048aaa7eff9f319cc6fd9
nciochina <ciochina_nicu@yahoo.com>
2021-03-22 14:30:26 +0200

    11

--- FirstTest.py
+++ FirstTest.py
@@ -3,3 +3,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://www.bnm.md/")
+driver.maximize_window()
+element = driver.find_element_by_xpath("//a[contains(text(),'Media')]")
+element.click()
+
