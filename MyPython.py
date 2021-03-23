ef47d96151ee09f4caff136d2fbe4f5242f25c2c
nicu <test@gmail.com>
2021-03-23 22:54:14 +0200

    2300

--- MyPython.py
+++ MyPython.py
@@ -1,3 +1,28 @@
-b = 3000
-a = "My age is"
-print(a, b)
+from selenium import webdriver 
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.webdriver.common.by import By
+
+link = "http://suninjuly.github.io/simple_form_find_task.html"
+
+try:
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser.get(link)
+
+    input1 = browser.find_element_by_tag_name("input")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_name("last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_class_name("form-control.city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element_by_id("country")
+    input4.send_keys("Russia")
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
