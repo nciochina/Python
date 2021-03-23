a56b8c50ecb526b0da8f6b17a488e4f006aec81f
nciochina <ciochina_nicu@yahoo.com>
2021-03-23 16:58:57 +0200

    1700

---
+++ Ubung.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("http://suninjuly.github.io/simple_form_find_task.html")
+button = driver.find_elements_by_id("submit_button")
+
+button.click()
+
