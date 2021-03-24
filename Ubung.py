0823e9114f5e45699125e0e26f238fea284aeabb
nciochina <ciochina_nicu@yahoo.com>
2021-03-24 10:30:32 +0200

    2403

--- Ubung.py
+++ Ubung.py
@@ -1,16 +1,16 @@
 from selenium import webdriver 
 import time
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.common.by import By
 
 link = "https://www.bnm.md/"
 try:
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(link)
+    driver.maximize_window()
     element = driver.find_element(By.XPATH, "//a[contains(text(),'Media')]")
     element.click()
 
 finally:
-    time.sleep(30)
+    time.sleep(5)
     driver.quit()
