55eec4afc5397314cbb49c1366ee2240930535b5
nicu <test@gmail.com>
2021-03-24 23:40:56 +0200

    2340

---
+++ FindByElements.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+
+try:
+    browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser.get("http://suninjuly.github.io/huge_form.html")
+    elements = browser.find_elements_by_tag_name("input")
+   
+    for element in elements:
+       element.send_keys("Мой ответ")
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    time.sleep(30)
+    browser.quit()
+
